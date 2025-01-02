6e94488911d3f9f6626508d23334557cb5a6940b
JOHNSON-MBILINYI <Keyjunior0061@gmail.com>
2025-01-02 17:37:26 +0300

    Update and rename 2.cpp to School MS.cpp

---
+++ School MS.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct Student {
+    int id;
+    string name;
+    int age;
+    string grade;
+};
+vector<Student> students;
+
+void addStudent() {
+    Student student;
+    cout << "Enter student ID: ";
+    cin >> student.id;
+    cout << "Enter student name: ";
+    cin.ignore();
+    getline(cin, student.name);
+    cout << "Enter student age: ";
+    cin >> student.age;
+    cout << "Enter student grade: ";
+    cin.ignore();
+    getline(cin, student.grade);
+
+    students.push_back(student);
+    cout << "Student added successfully!\n";
+}
+
+void viewStudents() {
+    if (students.empty())
+	 {
+        cout << "No students found.\n";
+        return;
+    }
+
+    cout << "List of Students:\n";
+    for (const auto& student : students) 
+	{
+        cout << "ID: " << student.id << ", Name: " << student.name
+             << ", Age: " << student.age << ", Grade: " << student.level << endl;
+    }
+}
+

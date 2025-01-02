592e802f6236341f1439e0a5a38a65818a39b548
JOHNSON-MBILINYI <Keyjunior0061@gmail.com>
2025-01-02 17:04:39 +0300

    Startin new project of hotel management system

---
+++ H1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+// Class ya Room
+class Room {
+public:
+    int roomNumber;
+    string roomType;
+    float price;
+    bool isBooked;
+
